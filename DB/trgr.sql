6df7f843efb6912938a7748569d6c99c4722f6f8
Ahmed Otifi <67879502+otifi3@users.noreply.github.com>
2025-02-09 14:54:22 +0200

    contribut trgr commit

--- DB/trgr.sql
+++ DB/trgr.sql
@@ -1,3 +1,4 @@
+-- Delete Wish trgr
 CREATE OR REPLACE TRIGGER trg_delete_wish
 BEFORE DELETE ON WISH
 FOR EACH ROW
@@ -16,4 +17,34 @@ BEGIN
     DELETE FROM CONTRIBUTION
     WHERE WISH_ID = :OLD.WISH_ID;
 END;
+/       
+
+-- contribut trgr
+CREATE OR REPLACE TRIGGER trg_before_insert_contribution
+BEFORE INSERT ON CONTRIBUTION
+FOR EACH ROW
+DECLARE
+    v_current_points NUMBER;
+BEGIN
+    -- Get the current points of the contributor
+    SELECT POINTS
+    INTO v_current_points
+    FROM USER_S
+    WHERE USER_NAME = :NEW.CONTRIBUTOR_NAME;
+
+    -- Check if the user has enough points
+    IF v_current_points < :NEW.AMOUNT THEN
+        -- Reject the insert if the user's points would become negative
+        RAISE_APPLICATION_ERROR(-20001, 'Insufficient points. Contribution amount exceeds available points.');
+    ELSE
+        -- Deduct the contribution amount from the user's points
+        UPDATE USER_S
+        SET POINTS = POINTS - :NEW.AMOUNT
+        WHERE USER_NAME = :NEW.CONTRIBUTOR_NAME;
+    END IF;
+END;
 /
+
+
+
+
